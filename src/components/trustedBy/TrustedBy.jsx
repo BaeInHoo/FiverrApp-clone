4f5491331e17c2cac79c5f03cc4b3026081e9ccc
BaeInHoo <qodlsgh0210@naver.com>
2023-05-20 22:43:13 +0900

    TrustedBy component design

---
+++ src/components/trustedBy/TrustedBy.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./TrustedBy.scss"
+
+const TrustedBy = () => {
+  return (
+    <div className='trusted-by'>
+      <div className="container">
+        <span>Trusted By!</span>
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default TrustedBy
