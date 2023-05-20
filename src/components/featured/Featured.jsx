02d84a224610b68fc0a3b856ddeb88ce779ae156
BaeInHoo <qodlsgh0210@naver.com>
2023-05-20 21:22:11 +0900

    Featured Component Design

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -7,7 +7,7 @@ const Featured = () => {
     <div className='featured'>
       <div className="container">
         <div className="left">
-          <h1>Find the perfect freelance services for your business</h1>
+          <h1>Find the perfect <i>freelance</i> services for your business</h1>
           <div className="search">
             <div className="searchInput">
               <BiSearch className='f-icons'/>
