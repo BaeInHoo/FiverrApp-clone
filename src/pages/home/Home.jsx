fe9cb0a848ccecfd0da7a0a3a93b56c835982ffb
BaeInHoo <qodlsgh0210@naver.com>
2023-05-23 19:15:16 +0900

    product slider

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import { BiCheck } from 'react-icons/bi'
 import CatCard from '../../components/catCard/CatCard'
 import Featured from '../../components/featured/Featured'
+import ProjectCard from '../../components/projectCard/ProjectCard'
 import Slide from '../../components/slide/Slide'
 import TrustedBy from '../../components/trustedBy/TrustedBy'
-import { cards } from '../../data'
+import { cards, projects } from '../../data'
 import "./Home.scss"
 
 const Home = () => {
@@ -95,6 +96,11 @@ const Home = () => {
           </div>
         </div>
       </div>
+      <Slide slidesToShow={4} arrowScroll={4}>
+        {projects.map(card => (
+          <ProjectCard key={card.id} item={card}/>
+        ))}
+      </Slide>
     </div>
   )
 }
