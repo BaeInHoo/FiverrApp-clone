73d2b02639ff59e5862a4e9be08bf4a188572ed9
BaeInHoo <qodlsgh0210@naver.com>
2023-05-27 21:21:41 +0900

    Gigs design

--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Gigs.scss"
+import { AiFillCaretDown } from "react-icons/ai"
 
 const Gigs = () => {
+  const [open, setOpen] = useState(false);
+  const [sort, setSort] = useState("sales");
+
+  const reSort = (type) => {
+    setSort(type)
+    setOpen(false)
+  }
+
   return (
-    <div>Gigs</div>
+    <div className='gigs'>
+      <div className="container">
+        <span className="breadcrumbs">FIVERR > GRAPHICS & DESIGN ></span>
+        <h1>AI Artists</h1>
+        <p>
+          Explore the boundaries of art and technology with Fiverr's AI artists
+        </p>
+        <div className="menu">
+          <div className="left">
+            <span>Budged</span>
+            <input type="text" placeholder='min'/>
+            <input type="text" placeholder='max'/>
+            <button>Apply</button>
+          </div>
+          <div className="right">
+            <span className='sort-by'>SortBy</span>
+            <span className='sort-type'>{sort === "sales" ? "Best Selling" : "Newest"}</span>
+            <AiFillCaretDown className="icons" onClick={()=>setOpen(!open)}/>
+            {open && (
+              <div className="right-menu">
+                <span onClick={()=>reSort("createdAt")}>Newest</span>
+                <span onClick={()=>reSort("sales")}>Best Selling</span>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
