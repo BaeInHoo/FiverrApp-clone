edd23ee49ee3b913785788361238c0374cf0d7a5
BaeInHoo <qodlsgh0210@naver.com>
2023-05-31 21:05:30 +0900

    MyGigs design

--- src/pages/myGIgs/MyGigs.jsx
+++ src/pages/myGIgs/MyGigs.jsx
@@ -9,7 +9,9 @@ const MyGigs = () => {
       <div className="container">
         <div className="title">
           <h1>Gigs</h1>
-          <Link to='/add'>Add</Link>
+          <Link to='/add'>
+            <button>Add New Gig</button>
+          </Link>
         </div>
         <table>
           <tr>
@@ -27,7 +29,62 @@ const MyGigs = () => {
             <td>88</td>
             <td>123</td>
             <td>
-              <AiFillDelete className='icons'/>
+              <AiFillDelete className='delete-icons'/>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img src="https://images.pexels.com/photos/8451166/pexels-photo-8451166.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <AiFillDelete className='delete-icons'/>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img src="https://images.pexels.com/photos/8451166/pexels-photo-8451166.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <AiFillDelete className='delete-icons'/>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img src="https://images.pexels.com/photos/8451166/pexels-photo-8451166.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <AiFillDelete className='delete-icons'/>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img src="https://images.pexels.com/photos/8451166/pexels-photo-8451166.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <AiFillDelete className='delete-icons'/>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img src="https://images.pexels.com/photos/8451166/pexels-photo-8451166.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <AiFillDelete className='delete-icons'/>
             </td>
           </tr>
         </table>
