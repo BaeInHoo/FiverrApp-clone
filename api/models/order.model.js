b72a6aefb8242f79d832485bb6df8ee021322884
BaeInHoo <qodlsgh0210@naver.com>
2023-06-06 20:03:24 +0900

    order model type 적립

--- api/models/order.model.js
+++ api/models/order.model.js
@@ -1,10 +1,41 @@
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-  
+const OrderSchema = new Schema({
+  gigId: {
+    type: String,
+    required: true,
+  },
+  img: {
+    type: String,
+    required: false,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  sellerId: {
+    type: String,
+    required: true,
+  },
+  buyerId: {
+    type: String,
+    required: true,
+  },
+  isCompleted: {
+    type: Boolean,
+    default: false,
+  },
+  payment_intent: {
+    type: String,
+    required: true,
+  },
 }, {
   timestamp: true
 });
 
-export default mongoose.model("User", userSchema);
+export default mongoose.model("Order", OrderSchema);
