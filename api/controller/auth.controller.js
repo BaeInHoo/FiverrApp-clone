8a6a0605d47fa1119397bbf6cf3fc5c588dd950b
BaeInHoo <qodlsgh0210@naver.com>
2023-06-08 15:05:27 +0900

    login 구현

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -16,8 +16,20 @@ export const register = async (req, res) => {
   }
 }
 
-export const login = (req, res) => {
-  
+export const login = async (req, res) => {
+  try {
+    const user = await User.findOne({username: req.body.username});
+    if (!user) return res.status(404).send("유저가 없습니다");
+
+    const isCorrect = bcrypt.compareSync(req.body.password, user.password);
+    if (!isCorrect) return res.status(400).send("비밀번호나 아이디가 틀렸습니다");
+
+    const {password, ...info} = user._doc
+    res.status(200).send(user)
+
+  } catch (error) {
+    res.status(500).send("잘못된 정보 입니다")
+  }
 }
 
 export const logout = (req, res) => {
