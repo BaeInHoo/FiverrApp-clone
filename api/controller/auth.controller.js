bc28cdc2a16ab05191229b79f1d2f68562e8a8d1
BaeInHoo <qodlsgh0210@naver.com>
2023-06-08 15:26:50 +0900

    import jwt and cookie-parser

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
   try {
@@ -23,6 +24,11 @@ export const login = async (req, res) => {
 
     const isCorrect = bcrypt.compareSync(req.body.password, user.password);
     if (!isCorrect) return res.status(400).send("비밀번호나 아이디가 틀렸습니다");
+
+    const token = jwt.sign({
+      id: user._id,
+      isSeller: user.isSeller,
+    });
 
     const {password, ...info} = user._doc
     res.status(200).send(user)
