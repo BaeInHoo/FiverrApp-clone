dce6928e18c64d97a7c5fa0d26253b47ab0ee645
BaeInHoo <qodlsgh0210@naver.com>
2023-06-11 20:15:19 +0900

    login page design

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -3,8 +3,26 @@ import "./Login.scss"
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
+    <div className="login">
+      <form>
+        <h1>Sign in</h1>
+        <label htmlFor="">Username</label>
+        <input
+          name="username"
+          type="text"
+          placeholder="johndoe"
+        />
+
+        <label htmlFor="">Password</label>
+        <input
+          name="password"
+          type="password"
+          placeholder='johndoe@gmail.com'
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login
