596a9bedd669d68ba5016d683a4391e52770b7af
BaeInHoo <qodlsgh0210@naver.com>
2023-06-21 19:12:49 +0900

    gig real-world 적용

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -11,3 +11,10 @@ export const deleteUser = async (req, res, next) => {
   res.status(200).send("삭제되었습니다")
 
 }
+
+export const getUser = async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  res.status(200).send(user)
+
+}
--- client/src/components/gigCard/GigCard.jsx
+++ client/src/components/gigCard/GigCard.jsx
@@ -2,21 +2,44 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import "./GigCard.scss"
 import { AiFillStar, AiFillHeart } from "react-icons/ai"
+import { useQuery } from '@tanstack/react-query'
+import newRequest from '../../utils/newRequest'
 
 const GigCard = ({ item }) => {
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["gigUser"],
+    queryFn: () =>
+      newRequest
+        .get(
+          `/users/${item.userId}`
+        )
+        .then((res) => {
+          return res.data;
+        }),
+  });
+
   return (
     <Link to="/gig/123" className="link">
       <div className="gig-card">
         <img src={item.img} alt="" />
         <div className="info">
-          <div className="user">
-            <img src={item.pp} alt="" />
-            <span>{item.username}</span>
-          </div>
+          {isLoading ? (
+              "loading"
+            ) : error ? (
+              "잘못된 접근 입니다"
+            ) : (
+              <div className="user">
+                <img src={data.img || "/img/noavater.jpg"} alt="" />
+                <span>{data.username}</span>
+              </div>
+          )}
           <p>{item.desc}</p>
           <div className="star">
             <AiFillStar className="icons"/>
-            <span>{item.star}</span>
+            <span>
+              {!isNaN(item.totalStars / item.starNumber) && 
+              Math.round(item.totalStars / item.starNumber)}
+            </span>
           </div>
         </div>
         <hr />
